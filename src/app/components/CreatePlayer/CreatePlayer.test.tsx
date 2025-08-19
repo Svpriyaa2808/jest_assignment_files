a463716c891c92e3202016bed6bf82340dc66a36
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-08-19 23:27:39 +0200

    changing state values

--- src/app/components/CreatePlayer/CreatePlayer.test.tsx
+++ src/app/components/CreatePlayer/CreatePlayer.test.tsx
@@ -5,7 +5,7 @@ import CreatePlayer from '.';
 describe('button tests', () => {
 
     test('should render a Start button', () => {
-        render(<CreatePlayer  />)
+        render(<CreatePlayer  setPlayer={() => { }}/>)
 
         const startBtn: HTMLElement = screen.getByRole('button', { name: /start game/i });
 
@@ -14,7 +14,7 @@ describe('button tests', () => {
 
     test('should render a disabled Start button if input is less than 3', () => {
         const inputTextMock: string = 'p'.repeat(2);
-        render(<CreatePlayer  />)
+        render(<CreatePlayer  setPlayer={() => { }}/>)
 
         const startBtn: HTMLElement = screen.getByRole('button', { name: /start game/i });
         const input: HTMLInputElement = screen.getByLabelText(/pick a name/i);  
@@ -26,7 +26,7 @@ describe('button tests', () => {
 
     test('should render a enabled Start button if input is more than 2', () => {
         const inputTextMock: string = 'p'.repeat(3);
-        render(<CreatePlayer  />)
+        render(<CreatePlayer  setPlayer={() => { }}/>)
 
         const startBtn: HTMLElement = screen.getByRole('button', { name: /start game/i });
         const input: HTMLInputElement = screen.getByLabelText(/pick a name/i);
@@ -36,12 +36,12 @@ describe('button tests', () => {
         expect(startBtn).not.toBeDisabled();
     })
 
-//  })
+})
 
-// describe('input tests', () => {
+describe('input tests', () => {
 
     test('should render a input component with empty default value', () => {
-        render(<CreatePlayer  />)
+        render(<CreatePlayer  setPlayer={() => { }}/>)
 
         const input: HTMLInputElement = screen.getByLabelText(/pick a name/i);
 
@@ -52,7 +52,7 @@ describe('button tests', () => {
     test('should only take 15 characters in inputfield', async () => {
         const inputTextMock: string = '1'.repeat(20);
         const user: UserEvent = userEvent.setup();
-        render(<CreatePlayer />)
+        render(<CreatePlayer setPlayer={() => { }}/>)
 
         let input: HTMLInputElement = screen.getByLabelText(/pick a name/i);
         await user.type(input, inputTextMock);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 'use client'
-import { useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 import Image from "next/image";
 import CreatePlayer from "./components/CreatePlayer";
 import GameHeader from "./components/GameHeader";
 
 export default function Home() {
- 
+  const[playerName,setPlayerName] = useState<string>('')
+  const [gamestart,setGameStart] = useState<boolean>(false)
+  const [inputValue,setInputValue] = useState<string>('')
+
+  const handleGame = () => {
+    setGameStart(true) 
+
+  }
+  
   return (
     <>
-    <CreatePlayer />
+    {!gamestart && 
+    <CreatePlayer setPlayer={handleGame}/>}
+    {gamestart && 
     <GameHeader player="tobias"/>
+}
     </>
   );
 }
